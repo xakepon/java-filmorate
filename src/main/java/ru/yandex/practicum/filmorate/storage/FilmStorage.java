c91d04760324e6f8a7ad5e093a3fe13cf3c8471c
Marat <xakeponus@yandex.ru>
2024-03-02 23:24:24 +0300

    v0

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -10,6 +10,8 @@ public interface FilmStorage {
     Film addFilm(Film film) throws ValidException;
 
     Film updateFilm(Film film) throws ValidException;
+
     List<Film> getAllFilms();
+
     List<Film> getPopularFilms(int count);
 }
