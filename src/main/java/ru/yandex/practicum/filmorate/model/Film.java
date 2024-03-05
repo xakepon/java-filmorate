65e5f8483c11a0f5234fbb19aa816a233feb0748
Marat <xakeponus@yandex.ru>
2024-03-06 01:09:47 +0300

    v0

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -6,7 +6,6 @@ import ru.yandex.practicum.filmorate.controller.MyDurationSerializer;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 
 import java.time.Duration;
