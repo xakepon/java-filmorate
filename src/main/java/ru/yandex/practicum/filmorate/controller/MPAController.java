099ab2ff6115e42142f7cd43174b3a35ff2bcd06
Marat <xakeponus@yandex.ru>
2024-04-20 19:04:26 +0300

    v0

--- src/main/java/ru/yandex/practicum/filmorate/controller/MPAController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/MPAController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.practicum.filmorate.service.FilmService;
-import ru.yandex.practicum.filmorate.storage.FilmStorage;
 
 import java.util.List;
 import java.util.Map;
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -9,10 +9,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import java.time.LocalDate;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
-
 @Data
 @Validated
 public class User {
